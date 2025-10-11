5875194e26b0e1d7ae7f4a02a96e367f12588e34
LENOVO <hasanovnicat1122@gmail.com>
2025-10-11 23:59:10 +0400

    new task

--- 3. Frontend-02/JavaScript/11. Task - 10/script.js
+++ 3. Frontend-02/JavaScript/11. Task - 10/script.js
@@ -3,10 +3,6 @@ let accordionContent = document.querySelectorAll(".accordion__content");
 
 accordionBtn.forEach((btn, i) => {
   btn.addEventListener("click", () => {
-    accordionContent.forEach((content) => {
-      content.classList.remove("active");
-    });
-
     accordionContent[i].classList.toggle("active");
   });
 });
